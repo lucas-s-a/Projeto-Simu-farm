901809f33eac467af9ad1b25a01273c659d70ffb
lucas-s-a <lucassalves485@outlook.com>
2022-12-14 11:55:50 -0200

    edição 7

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,33 +1,49 @@
 import { Outlet, Link } from "react-router-dom";
+import {useState,useEffect} from "react";
+import Loading from "../componentes/Loading"
 
 const Layout = () => {
-  return (
-    <>
-      <header>
-        <div className="emlinha2">
-          <div className="i2">
-            <img className="posimg" width="100px" src='' alt="logo"/>
-          </div>
-          <div className="i2">
-            <div>
-              <ul className="emlinhas">
-                <Link to="/"><li className="itens">Home</li></Link>
-                <Link to="/Get"><li className="itens">Ver Lista</li></Link>
-                <li className="itens">
-                  Contato
-                </li>
-                <li className="itens">
-                  Sobre
-                </li>
-              </ul>
+  const [loading, setLoading] = useState(true);
+    
+  useEffect(() => {
+    const loadData = async () => {
+      await new Promise((r) => setTimeout(r, 2000));
+      setLoading((loading) => !loading);
+    };
+      
+    loadData();
+  }, [])
+  if (loading) {
+    return <div className="caixagrid"><div className="elemento">{<Loading />}</div></div>
+  }else{
+    return (
+      <>
+        <header>
+          <div className="blocop">
+            <div className="ebloco1">
+              <img className="bloco1" width="100px" src='' alt="logo"/>
+            </div>
+            <div className="ebloco2">
+              <div>
+                <ul className="bloco2">
+                  <Link to="/"><li>Home</li></Link>
+                  <Link to="/Get"><li>Lista de Produtos</li></Link>
+                  <li>
+                    Contato
+                  </li>
+                  <li>
+                    Sobre
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-      </header>      
-
-      <Outlet />
-    </>
-  )
+        </header>      
+  
+        <Outlet />
+      </>
+    )
+  }
 };
 
 export default Layout;
--- src/pages/Ver.js
+++ src/pages/Ver.js
@@ -1,12 +1,22 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { Link } from "react-router-dom";
-import Loading from "./Loading"
+import Loading from "../componentes/Loading"
 
 const Listar = () => {
   const [dados,setDados] = useState([]);
   const [barra,setBarra] = useState('');
+  const [loading, setLoading] = useState(true);
   const [removerLoading,setRemoverLoading] = useState(false)
+
+  useEffect(() => {
+    const loadData = async () => {
+      await new Promise((r) => setTimeout(r, 2000));
+      setLoading((loading) => !loading);
+    };
+      
+    loadData();
+  }, [])
   
   let Url = `https://projeto-apredendoo-servidor-json.vercel.app/`+barra;
   function Cdados() {
@@ -18,68 +28,75 @@ const Listar = () => {
       });
     },2000)
   }
-  return (<>
-  <div className="container-grid">
-    <header className="itemheader">
-      <h1>Veja aqui o itens que estão na sua lista de desejos</h1>
-    </header>
-    <br></br>
-    <main>
-      <div className="caixa">
-        <div className="item1">
-          <h1>Buscar Dados</h1>
-          <select onChange={(e)=>{const selecao = e.target.value;setBarra(selecao)}}>
-            <option value="Erro">---------</option>
-            <option value="Medicamentos">Medicamentos</option>
-            <option value="Vitaminas">Vitaminas</option>
-            <option value="Lanches_Rapidos">Lanches Rápidos</option>
-            <option value="Acessorios_Treino">Acessorios Treino</option>
-            <option value="Tratamentos_Capilares">Tratamentos Capilares</option>
-            <option value="Saude_Sexual">Público Masculino</option>
-            <option value="Ortopedicos">Ortopedicos</option>
-            <option value="Alimentacao_infantil">Alimentacao Infantil</option>
-            <option value="Suplementos_Academia">Suplementos Academia</option>
-            <option value="Beleza">Beleza</option>
-            <option value="Cabelo">Cabelo</option>
-            <option value="Higiene-Pessoal">Higiene Pessoal</option>
-          </select>
-          <div>
-            <br></br>
-            <button onClick={Cdados}>Buscar</button>
-            <br></br>
+  if (loading) {
+    return <div className="caixagrid"><div className="elemento"><Loading /></div></div>
+  }
+  else{
+    return (<>
+      <div className="container-grid">
+        <header className="itemheader">
+          <h1>Veja aqui o itens que estão na sua lista de desejos</h1>
+        </header>
+        <br></br>
+        <main>
+          <div className="caixa">
+            <div className="item1">
+              <h1>Buscar Dados</h1>
+              <select onChange={(e)=>{const selecao = e.target.value;setBarra(selecao)}}>
+                <option value="Erro">---------</option>
+                <option value="Medicamentos">Medicamentos</option>
+                <option value="Vitaminas">Vitaminas</option>
+                <option value="Lanches_Rapidos">Lanches Rápidos</option>
+                <option value="Acessorios_Treino">Acessorios Treino</option>
+                <option value="Tratamentos_Capilares">Tratamentos Capilares</option>
+                <option value="Saude_Sexual">Público Masculino</option>
+                <option value="Ortopedicos">Ortopedicos</option>
+                <option value="Alimentacao_infantil">Alimentacao Infantil</option>
+                <option value="Suplementos_Academia">Suplementos Academia</option>
+                <option value="Beleza">Beleza</option>
+                <option value="Cabelo">Cabelo</option>
+                <option value="Higiene-Pessoal">Higiene Pessoal</option>
+              </select>
+              <div>
+                <br></br>
+                <button onClick={Cdados}>Buscar</button>
+                <br></br>
+              </div>
+            </div>
+            <div className="item2">
+              <button><Link to={"/Post"} >Adicionar Novos itens</Link></button>
+            </div>
           </div>
-          {removerLoading && <Loading />}
-        </div>
-        <div className="item2">
-          <button><Link to={"/Post"} >Adicionar Novos itens</Link></button>
-        </div>
+          <div className="caixagridcarregar">
+            <div className="elemento">{removerLoading && <Loading />}</div>
+          </div>
+          <hr></hr>
+          <div className="caixadedados">
+          {dados.map((data) => {
+            return <div key={data.id} className="caixadeinformação">
+                <div className="imag">
+                  <img width="100px" height="100px" src={data.imgl} alt="imagenspegadas"/>
+                </div>
+                <div className="dadostexto">
+                  <ul>
+                    <li>{data.nome}</li>
+                    <li>{data.descricao}</li>
+                    <li>{data.preco}</li>
+                  </ul>
+                </div>
+                <div className="links">
+                  <Link to={`/Editar/${barra}/${data.id}`}><button className="linktamanho">Editar</button></Link>
+                </div>
+                <div className="links">
+                  <Link to={`/Deletar/${barra}/${data.id}`}><button className="linktamanho">Deletar</button></Link>
+                </div>
+            </div> 
+          })}
+          </div>
+        </main>
       </div>
-      <hr></hr>
-      <div className="caixadedados">
-      {dados.map((data) => {
-        return <div key={data.id} className="caixadeinformação">
-            <div className="imag">
-              <img width="100px" height="100px" src={data.imgl} alt="imagenspegadas"/>
-            </div>
-            <div className="dadostexto">
-              <ul>
-                <li>{data.nome}</li>
-                <li>{data.descricao}</li>
-                <li>{data.preco}</li>
-              </ul>
-            </div>
-            <div className="links">
-              <Link to={`/Editar/${barra}/${data.id}`}><button className="linktamanho">Editar</button></Link>
-            </div>
-            <div className="links">
-              <Link to={`/Deletar/${barra}/${data.id}`}><button className="linktamanho">Deletar</button></Link>
-            </div>
-        </div> 
-      })}
-      </div>
-    </main>
-  </div>
-  
-  </>)
+      
+      </>)
+  }
 };
 export default Listar;
